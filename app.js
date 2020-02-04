1c8f3ca5eff792a2918a19183f8cbdfa8d74af86
Andrew Reynolds <andrew@nearablecorp.com>
2020-02-04 11:11:29 -0800

    randomPhraseAsArray function created

---
+++ app.js
@@ -0,0 +1,37 @@
+// Variables for guessing app
+
+
+let missed = 0;
+
+const qwerty = document.querySelector('#qwerty');
+const phrase = document.querySelector('#phrase');
+const overlay = document.querySelector('#overlay');
+const gameStartButton = document.querySelector('.btn__reset');
+
+let phrases = ["coding is great", "you can do it", "treehouse is wonderful", "front end rocks", "build a solid base"];
+
+// Functions: 
+
+function getRandomPhraseAsArray (arr){
+  let arrayLength = arr.length;
+  function randomNum (topNum=arrayLength){
+    return Math.floor(Math.random() * topNum);
+  };
+  let chosenSentence = phrases[randomNum()];
+  let arrayOfCharacters = chosenSentence.split('');
+  return arrayOfCharacters;  
+};
+
+
+
+//  Event Handelers: 
+
+overlay.addEventListener('click', (e)=>{
+  let startLink = e.target;
+  if(startLink.className === 'btn__reset')
+  overlay.style.display = 'none'
+
+});
+
+
+
